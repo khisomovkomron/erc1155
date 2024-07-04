5b8a368516619c2b2ada79e08474585585014982
Komron Khisomov <normok_9595@bk.ru>
2024-07-04 13:48:11 +0500

    fixed some issues

--- test/MultiToken.t.sol
+++ test/MultiToken.t.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {DeployMultiToken} from "../script/Deploy_MultiToken.sol";
+import {MultiToken} from "../src/MultiToken.sol";
 
 contract MultiTokenTest is Test {
-    DeployMultiToken public multiToken;
+    MultiToken public multiToken;
 
     function setUp() public {
-        DeployMultiToken multitoken = new DeployMultiToken();
+        DeployMultiToken deploy_multitoken = new DeployMultiToken();
 
-        token = multitoken.run()
+        multiToken = deploy_multitoken.run();
     }
 
 }
